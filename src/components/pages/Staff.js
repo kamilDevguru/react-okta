f4acfcc36bd702593ba0aba1d0e984a7278a8c21
Jonathan Bogdanowicz <kamil.czoska1997@outlook.com>
2019-09-08 01:26:44 -0300

    first commit

---
+++ src/components/pages/Staff.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Staff extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Staff</h1>
+      </div>
+    );
+  }
+}
+
+export default Staff;
